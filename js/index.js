445a7cad3b1dc113b9a18843aa7d481e1f5180d4
Sidhant Samant <47266013+SidhantSamant@users.noreply.github.com>
2020-03-26 15:05:10 +0530

    themed version

--- js/index.js
+++ js/index.js
@@ -10,3 +10,34 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+
+//theme
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+//memory to hold theme while refresh
+const theme = localStorage.getItem('theme');
+
+if(theme){
+    body.classList.add(theme);
+}
+
+//theme button handler
+darkButton.onclick = () => {
+ body.classList.replace('light','dark') ||  body.classList.replace('solar','dark');
+ localStorage.setItem('theme','dark');
+
+};
+
+lightButton.onclick = () => {
+ body.classList.replace('dark','light') ||  body.classList.replace('solar','light') ;
+ localStorage.setItem('theme','light');
+};
+
+solarButton.onclick = () => {
+    body.classList.replace('light','solar') || body.classList.replace('dark','solar');
+    localStorage.setItem('theme','solar');
+};
