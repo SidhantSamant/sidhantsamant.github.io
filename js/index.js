fffb826f52ac27a0d1b0fd2ebb629a1cc2a37099
Sidhant Samant <47266013+SidhantSamant@users.noreply.github.com>
2021-08-10 15:25:43 +0530

    js file updated

--- js/index.js
+++ js/index.js
@@ -1,77 +1,53 @@
-//menu-button
-const navToggle = document.querySelector(".toggle-button");
-navToggle.addEventListener("click", () => {
-	document.body.classList.toggle("nav-open");
-});
-
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-const navlink0 = document.getElementsByClassName("navlink0")[0];
-const navlink1 = document.getElementsByClassName("navlink1")[0];
-const navlink2 = document.getElementsByClassName("navlink2")[0];
-const navlink3 = document.getElementsByClassName("navlink3")[0];
-const navlink4 = document.getElementsByClassName("navlink4")[0];
+//Navbar buttons
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+const navLink0 = document.getElementsByClassName("navlink")[0];
+const navLink1 = document.getElementsByClassName("navlink")[1];
+const navLink2 = document.getElementsByClassName("navlink")[2];
+const navLink3 = document.getElementsByClassName("navlink")[3];
 
 toggleButton.addEventListener("click", () => {
 	navbarLinks.classList.toggle("active");
+	document.body.classList.toggle("nav-open");
 });
-
-navlink0.addEventListener("click", () => {
+navLink0.addEventListener("click", () => {
 	navbarLinks.classList.remove("active");
 	document.body.classList.remove("nav-open");
 });
-navlink1.addEventListener("click", () => {
+navLink1.addEventListener("click", () => {
 	navbarLinks.classList.remove("active");
 	document.body.classList.remove("nav-open");
 });
-navlink2.addEventListener("click", () => {
+navLink2.addEventListener("click", () => {
 	navbarLinks.classList.remove("active");
 	document.body.classList.remove("nav-open");
 });
-navlink3.addEventListener("click", () => {
-	navbarLinks.classList.remove("active");
-	document.body.classList.remove("nav-open");
-});
-navlink4.addEventListener("click", () => {
+navLink3.addEventListener("click", () => {
 	navbarLinks.classList.remove("active");
 	document.body.classList.remove("nav-open");
 });
 
 //dark mode
 let darkMode = localStorage.getItem("darkMode");
-
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
-
 const enableDarkMode = () => {
-	// 1. Add the class to the body
 	document.body.classList.add("darkmode");
-	// 2. Update darkMode in localStorage
 	localStorage.setItem("darkMode", "enabled");
 };
-
 const disableDarkMode = () => {
-	// 1. Remove the class from the body
 	document.body.classList.remove("darkmode");
-	// 2. Update darkMode in localStorage
 	localStorage.setItem("darkMode", null);
 };
-
 // If the user already visited and enabled darkMode
-// start things off with it on
 if (darkMode === "enabled") {
 	enableDarkMode();
 }
-
 // When someone clicks the button
 darkModeToggle.addEventListener("click", () => {
-	// get their darkMode setting
 	darkMode = localStorage.getItem("darkMode");
 
-	// if it not current enabled, enable it
 	if (darkMode !== "enabled") {
 		enableDarkMode();
-		// if it has been enabled, turn it off
 	} else {
 		disableDarkMode();
 	}
